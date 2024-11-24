197a9343fe425a5a4c4756c681895f62712814aa
Laith <laithyaseen955@gmail.com>
2024-11-24 14:16:46 +0300

    edit

--- main.py
+++ main.py
@@ -1,52 +1,41 @@
-from flask import Flask , render_template , request , jsonify
+from flask import Flask, render_template, request, redirect, url_for  
 from keras.preprocessing.image import load_img
 from tensorflow.keras.models import load_model  
 from keras.preprocessing.image import img_to_array
 import numpy as np
 import os 
-
 import tensorflow as tf
-
 
 model = tf.keras.models.load_model('model/Alzheimer_Final_Model_forReal.keras')
 
-    
 app = Flask(__name__) 
 
 @app.route('/' , methods=['GET'])
 def Load_front():
-    return render_template('index.html')
+    prediction_message = request.args.get('prediction', None)
+    return render_template('index.html', prediction=prediction_message)
 
 
 @app.route('/' , methods=['POST'])
 def predict():
-
-
     app.config['UPLOAD_FOLDER'] = './images/'
 
     if 'imageFile' not in request.files or request.files['imageFile'].filename == '':
         prediction_message = "You must upload a photo."
         return render_template('index.html', prediction=prediction_message)
     
-    
     imageFile = request.files['imageFile']
     imgPath = './images/' + imageFile.filename
     imageFile.save(imgPath)
-
-
-   
 
     img = load_img(imgPath , target_size = (256 , 256))
     image = img_to_array(img)
     image = np.expand_dims(image, axis=0) 
     image /= 255.0
-    
-
 
     predictions = model.predict(image)
     predicted_class = np.argmax(predictions, axis=1)[0]
 
-    
     if predicted_class == 0:
         prediction_message = "Mild Alzheimer"
     elif predicted_class == 1:
@@ -56,10 +45,13 @@ def predict():
     elif predicted_class == 3:
         prediction_message = "Very mild Alzheimer"
     elif predicted_class == 4:
-        prediction_message = "Unknown class , Please make sure that you uploaded a mri brain photo"
+        prediction_message = "Unknown class, Please make sure that you uploaded a MRI brain photo"
 
+    
+    if os.path.exists(imgPath):
+        os.remove(imgPath)
 
-    return render_template('index.html' ,  prediction = prediction_message)
+    return redirect(url_for('Load_front', prediction=prediction_message))
 
 
 if __name__ == '__main__':
